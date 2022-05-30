cd6eaa16fb9615d86c544f7aa6f8b540f0138ae4
maxcutlyp <max@maxcutlyp.com>
2022-05-31 01:38:51 +0800

    add support for paging with -p or --pager

--- cmd/query.go
+++ cmd/query.go
@@ -3,17 +3,25 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/satoqz/lyr/query"
 	"github.com/spf13/cobra"
 )
 
+var Pager string
+
 var queryCmd = &cobra.Command{
 	Use:     "query",
 	Aliases: []string{"get", "find", "search"},
 	Args:    cobra.MinimumNArgs(1),
 	RunE:    queryExec,
+}
+
+func init() {
+    queryCmd.Flags().StringVarP(&Pager, "pager", "p", "", "Optionally specify a pager to use (e.g. less)")
 }
 
 func queryExec(_ *cobra.Command, args []string) error {
@@ -52,6 +60,20 @@ func queryExec(_ *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s\n\n%s\n", song.Name, lyrics)
-	return nil
+
+    if Pager == "" {
+        fmt.Printf("%s\n\n%s\n", song.Name, lyrics)
+    } else {
+        split := strings.Split(Pager, " ")
+        pager_cmd := exec.Command(split[0], split[1:]...)
+        pager_cmd.Stdin = strings.NewReader(song.Name + "\n\n" + lyrics)
+        pager_cmd.Stdout = os.Stdout
+
+        err := pager_cmd.Run()
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
 }
