7b7d1d132585a4a42a4c71a64de95173abcebb77
maxcutlyp <max@maxcutlyp.com>
2022-05-30 17:51:24 +0800

    update scraper

--- query/scrape.go
+++ query/scrape.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+    "bytes"
 
 	"github.com/PuerkitoBio/goquery"
+    "golang.org/x/net/html"
 )
 
 func (data queryData) ScrapeLyrics() (lyrics string, err error) {
 
 	attempt := func() (string, error) {
-
 		res, err := http.Get("https://genius.com" + data.Path)
 		if err != nil {
 			return "", err
@@ -29,11 +30,47 @@ func (data queryData) ScrapeLyrics() (lyrics string, err error) {
 			return "", err
 		}
 
-		return strings.Trim(
-			doc.Find(".lyrics").
-				First().
-				Text(),
-			"\n "), nil
+        // inspired by goquery's Selection.Text() method but extended to support bold, italics, and newlines
+        // https://github.com/PuerkitoBio/goquery/blob/master/property.go#L62
+        var buf bytes.Buffer
+        var parse_node func(*html.Node)
+        parse_node = func(node *html.Node) {
+            switch node.Type {
+                case html.TextNode:
+                    buf.WriteString(node.Data)
+                    return
+                case html.ElementNode:
+                    var open_esc_code string
+                    var close_esc_code string
+
+                    switch node.Data {
+                        case "br":
+                            buf.WriteString("\n")
+                            return
+                        case "b":
+                            open_esc_code = "\033[1m"
+                            close_esc_code = "\033[22m"
+                        case "i":
+                            open_esc_code = "\033[3m"
+                            close_esc_code = "\033[23m"
+                    }
+
+                    if node.FirstChild != nil {
+                        buf.WriteString(open_esc_code)
+                        for child := node.FirstChild; child != nil; child = child.NextSibling {
+                            parse_node(child)
+                        }
+                        buf.WriteString(close_esc_code)
+                    }
+            }
+        }
+
+        sel := doc.Find("[data-lyrics-container=\"true\"]").First()
+        for _, node := range sel.Nodes {
+            parse_node(node)
+        }
+
+		return strings.Trim(buf.String(), "\n "), nil
 	}
 
 	for lyrics == "" {
